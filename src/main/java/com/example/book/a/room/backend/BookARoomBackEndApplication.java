d114aace753e30a09353c0f55b0e6b8523be8405
olili27 <timmolili27@gmail.com>
2023-07-20 16:51:32 +0300

    initialise the project

---
+++ src/main/java/com/example/book/a/room/backend/BookARoomBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.book.a.room.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookARoomBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookARoomBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book/a/room/backend/BookARoomBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book.a.room.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookARoomBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
